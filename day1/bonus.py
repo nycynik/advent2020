076c66a108ad4547c19c793a22a5ad8d3acd54a9
Mike Lynch <nycynik@gmail.com>
2020-12-16 20:41:18 -0800

    day 1 part 2 done!

---
+++ day1/bonus.py
@@ -0,0 +1,26 @@
+# just going to brute force this. Need to find two numbers that sum to 2020
+import sys
+
+data_file = open("data.txt")
+
+all_values = []
+for val in data_file:
+    all_values.append(int(val))
+
+total_values = len(all_values)
+
+if total_values > 0:
+
+    for ii in range(0, total_values):
+        for ix in range(1, total_values):
+            for iy in range(2, total_values):
+                if ii % 100 == 0:
+                    sys.stdout.write(".")
+                if all_values[ii] + all_values[ix] + all_values[iy] == 2020:
+                    print(
+                        f"\nfound that {all_values[ii]} + {all_values[ix]} = 2020\nTheir product is {all_values[ii] * all_values[ix] *all_values[iy]}")
+
+else:
+    print("\ncould not read values")
+
+data_file.close()
