bc161189997363dbf5fdefdfc3ef8b9e7ed85a29
Mike Lynch <nycynik@gmail.com>
2020-12-17 23:13:55 -0800

    adding a template to speed up the days.

---
+++ template_solve.py
@@ -0,0 +1,51 @@
+
+def get_data():
+    data = []
+
+    data_file = open("data.txt")
+
+    for val in data_file:
+        data.append(val)
+    data_file.close()
+
+    return data
+
+
+def check_data(data):
+    return None
+
+
+def main():
+    data = get_data()
+    check_data(data)
+
+
+main()
+
+
+# with hash instead of array
+
+def get_data():
+    data = {}
+
+    data_file = open("data.txt")
+
+    for idx, val in enumerate(data_file):
+        data[idx] = val.split()
+    data_file.close()
+
+    return data
+
+
+def check_data(data):
+    for k, d in data.items():
+        print(k, d)
+    return None
+
+
+def main():
+    data = get_data()
+    check_data(data)
+
+
+main()
