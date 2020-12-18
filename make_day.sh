250deaaa042cbb3d1912a5d453007f61238df339
Mike Lynch <nycynik@gmail.com>
2020-12-17 23:33:27 -0800

    added script to make days - faster day creation - win

--- make_day.sh
+++ make_day.sh
@@ -4,6 +4,8 @@ if [ ! -d day$1 ]; then
   mkdir day$1
   cd day$1
   touch data.txt
+  touch test.txt
+  # xsel -b > data.txt
   cp ../template_solve.py ./solve.py
 else
   echo That directory exists
